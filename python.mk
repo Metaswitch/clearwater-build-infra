1af90614fc66c910ac46e354ae5c1d340e7b162e
Sebastian Rex <sebastian.rex@metaswitch.com>
2017-09-27 16:12:53 +0000

    Initial commit of common python makefile additions

--- python.mk
+++ python.mk
@@ -17,6 +17,72 @@ PIP := ${ENV_DIR}/bin/pip
 FLAKE8 := ${ENV_DIR}/bin/flake8
 BANDIT := ${ENV_DIR}/bin/bandit
 
+${PYTHON} ${ENV_DIR} ${PIP}:
+	# Set up a fresh virtual environment and install pip
+	virtualenv --setuptools --python=$(PYTHON_BIN) $(ENV_DIR)
+	$(ENV_DIR)/bin/easy_install "setuptools==24"
+
+${ENV_DIR}/.wheel: ${PIP}
+	# Ensure we have an up to date version of pip with wheel support
+	${PIP} install --upgrade pip==9.0.1
+	${PIP} install wheel==0.30.0
+	touch $@
+
+# Common rules to build a python component
+#
+# @param $1 - Target name
+#
+# Each target must supply:
+#   - <target>_SETUP        - A list of the setup.py files that the target depends on
+#   - <target>_REQUIREMENTS - A list of the requirements files that the target uses
+#   - <target>_SOURCES      - A list of the python source files that go into the wheel
+#   - <target>_WHEELS       - A list of the wheels that are produced
+#   - <target>_FLAGS        - (Optional) Extra flags to be passed to python commands
+#
+# The location of the wheelhouse can be overridden if desired, by specifying
+# <target>_WHEELHOUSE
+define python_component
+
+# The wheelhouse can be overridden if desired
+$1_WHEELHOUSE ?= $1_wheelhouse
+
+build-wheels: ${ENV_DIR}/.$1-build-wheels
+install-wheels: ${ENV_DIR}/.$1-install-wheels
+
+# Builds the wheels for this target
+${ENV_DIR}/.$1-build-wheels: $${$1_SETUP} $${$1_SOURCES} ${ENV_DIR}/.wheel
+	# For each setup.py file, generate the wheel
+	$$(foreach setup, $${$1_SETUP}, \
+		$${$1_FLAGS} ${PYTHON} $${setup} build -b build_$$(subst .py,,$${setup}) bdist_wheel -d $${$1_WHEELHOUSE};)
+
+	touch $$@
+
+# Downloads required dependencies and installs them in the local environment
+${ENV_DIR}/.$1-install-wheels: ${ENV_DIR}/.$1-build-wheels $${$1_REQUIREMENTS}
+	# Download the required dependencies
+	$${$1_FLAGS} ${PIP} wheel -w $${$1_WHEELHOUSE} $$(foreach req,$${$1_REQUIREMENTS},-r $${req}) --find-links $${$1_WHEELHOUSE}
+
+	# Install the required dependencies in the local environment
+	${INSTALLER} --find-links=$${$1_WHEELHOUSE} $${$1_WHEELS}  $$(foreach req,$${$1_REQUIREMENTS},-r $${req})
+
+	touch $$@
+
+endef
+
+
+${FLAKE8}: ${ENV_DIR} ${PIP}
+	${PIP} install flake8
+
+# TODO may need to add exlude list for etcd makefile
+verify: ${FLAKE8}
+	${FLAKE8} --select=E10,E11,E9,F "${FLAKE8_INCLUDE_DIR}" --exclude "${FLAKE8_EXCLUDE_DIR}"
+
+style: ${FLAKE8}
+	${FLAKE8} --select=E,W,C,N --max-line-length=100 "${FLAKE8_INCLUDE_DIR}"
+
+explain-style: ${FLAKE8}
+	${FLAKE8} --select=E,W,C,N --show-pep8 --first --max-line-length=100 "${FLAKE8_INCLUDE_DIR}"
+
 ${BANDIT}: ${ENV_DIR} ${PIP}
 	${PIP} install bandit
 
