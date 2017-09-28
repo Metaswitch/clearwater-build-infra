5b064845417a5435708ba7871cf2a9e047cea529
Sebastian Rex <sebastian.rex@metaswitch.com>
2017-09-28 13:41:15 +0000

    More python.mk tweaks

--- python.mk
+++ python.mk
@@ -9,7 +9,6 @@
 # Otherwise no rights are granted except for those provided to you by
 # Metaswitch Networks in a separate written agreement.
 
-.PHONY: analysis
 
 # Common definitions
 PYTHON := ${ENV_DIR}/bin/python
@@ -17,16 +16,20 @@ PIP := ${ENV_DIR}/bin/pip
 FLAKE8 := ${ENV_DIR}/bin/flake8
 BANDIT := ${ENV_DIR}/bin/bandit
 
+INSTALLER := ${PIP} install --compile \
+                            --no-index \
+                            --upgrade \
+                            --pre \
+                            --force-reinstall
+
 ${PYTHON} ${ENV_DIR} ${PIP}:
 	# Set up a fresh virtual environment and install pip
 	virtualenv --setuptools --python=$(PYTHON_BIN) $(ENV_DIR)
 	$(ENV_DIR)/bin/easy_install "setuptools==24"
 
-${ENV_DIR}/.wheel: ${PIP}
 	# Ensure we have an up to date version of pip with wheel support
 	${PIP} install --upgrade pip==9.0.1
 	${PIP} install wheel==0.30.0
-	touch $@
 
 # Common rules to build a python component
 #
@@ -46,11 +49,14 @@ define python_component
 # The wheelhouse can be overridden if desired
 $1_WHEELHOUSE ?= $1_wheelhouse
 
+.PHONY: build-wheels
 build-wheels: ${ENV_DIR}/.$1-build-wheels
+
+.PHONY: install-wheels
 install-wheels: ${ENV_DIR}/.$1-install-wheels
 
 # Builds the wheels for this target
-${ENV_DIR}/.$1-build-wheels: $${$1_SETUP} $${$1_SOURCES} ${ENV_DIR}/.wheel
+${ENV_DIR}/.$1-build-wheels: $${$1_SETUP} $${$1_SOURCES} ${PYTHON}
 	# For each setup.py file, generate the wheel
 	$$(foreach setup, $${$1_SETUP}, \
 		$${$1_FLAGS} ${PYTHON} $${setup} build -b build_$$(subst .py,,$${setup}) bdist_wheel -d $${$1_WHEELHOUSE};)
@@ -73,26 +79,22 @@ endef
 ${FLAKE8}: ${ENV_DIR} ${PIP}
 	${PIP} install flake8
 
-# TODO may need to add exlude list for etcd makefile
 verify: ${FLAKE8}
 	${FLAKE8} --select=E10,E11,E9,F "${FLAKE8_INCLUDE_DIR}" --exclude "${FLAKE8_EXCLUDE_DIR}"
 
 style: ${FLAKE8}
 	${FLAKE8} --select=E,W,C,N --max-line-length=100 "${FLAKE8_INCLUDE_DIR}"
 
+# TODO the "--show-pep8" option has been removed from flake8
 explain-style: ${FLAKE8}
 	${FLAKE8} --select=E,W,C,N --show-pep8 --first --max-line-length=100 "${FLAKE8_INCLUDE_DIR}"
 
 ${BANDIT}: ${ENV_DIR} ${PIP}
 	${PIP} install bandit
 
+.PHONY: analysis
 analysis: ${BANDIT}
 	# Scanning python code recursively for security issues using Bandit.
 	# Files in -x are ignored and only high severity level (-lll) are shown.
 	${ENV_DIR}/bin/bandit -r . -x "${BANDIT_EXCLUDE_LIST}" -lll
 
-INSTALLER := ${PIP} install --compile \
-                            --no-index \
-                            --upgrade \
-                            --pre \
-                            --force-reinstall
