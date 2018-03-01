d6635a8f4ca3f07fce486849ccb58a3a0805edbe
Ying-metaswitch <Ying.Huang@metaswitch.com>
2018-03-01 17:02:18 +0000

    feedback

--- doxygen.mk
+++ doxygen.mk
@@ -7,6 +7,23 @@
 
 .PHONY: doxygen
 doxygen:
-	sed -e "s/^OUTPUT_DIRECTORY .*/OUTPUT_DIRECTORY = ${DOCS_DIR}/g" build-infra/doxygen_config > build/doxygen_config
-	doxygen build/doxygen_config
+	doxygen -g ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^RECURSIVE .*/RECURSIVE = YES/g" ${BUILD_DIR}/Doxyfile
+
+	sed -i -e "s/^OUTPUT_DIRECTORY .*/OUTPUT_DIRECTORY = ${DOCS_DIR}/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^PROJECT_NAME .*/PROJECT_NAME = ${PROJECT_NAME}/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^PROJECT_NUMBER .*/PROJECT_NUMBER = ${git rev-parse HEAD}/g" ${BUILD_DIR}/Doxyfile
+
+	sed -i -e "s/^FORCE_LOCAL_INCLUDES .*/FORCE_LOCAL_INCLUDES = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^EXTRACT_ALL .*/EXTRACT_ALL = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^EXTRACT_PRIVATE .*/EXTRACT_PRIVATE = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^EXTRACT_PACKAGE .*/EXTRACT_PACKAGE = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^EXTRACT_STATIC .*/EXTRACT_STATIC = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^EXTRACT_LOCAL_METHODS .*/EXTRACT_LOCAL_METHODS = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^EXTRACT_ANON_NSPACES .*/EXTRACT_ANON_NSPACES = YES/g" ${BUILD_DIR}/Doxyfile
+	sed -i -e "s/^BUILTIN_STL_SUPPORT .*/BUILTIN_STL_SUPPORT = YES/g" ${BUILD_DIR}/Doxyfile
+	
+	sed -i -e "s/^LATEX_OUTPUT .*/LATEX_OUTPUT = NO/g" ${BUILD_DIR}/Doxyfile
+
+	doxygen ${BUILD_DIR}/Doxyfile
 
